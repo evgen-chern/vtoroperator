819683f3afac26ccecef007d681678a0e30cd955
evgen <57286862+evgen-chern@users.noreply.github.com>
2023-04-11 14:13:24 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    $('.change_lang').on('click', function (){
+        $(this).toggleClass('active');
+        $('.lang_list').stop(true).slideToggle();
+    });
+
+    let dropLink = '.dropdown_item';
+    $(dropLink).hover(function (){
+        $(this).children('.drop_link').toggleClass('active');
+        $(this).children('.dropdown_menu').stop(true).slideToggle();
+    });
+
+    let dropMobile = '.drop_link__mobile';
+    let dropMenuMobile = '.dropdown_menu__mobile';
+    $(document).on('click',function (e) {
+        if (!$(e.target).closest('.change_lang').length) {
+            $('.lang_list').stop(true).slideUp();
+            $('.change_lang').removeClass('active');
+        }
+        if (!$(e.target).closest(dropMobile).length) {
+            $(dropMenuMobile).stop(true).slideUp();
+            $(dropMobile).removeClass('active');
+        }
+    });
+
+    $('.header_mobile__button').on('click',function (){
+        $(this).toggleClass('active');
+        $('.header_mobile__menu_wrapper').stop(true).slideToggle(400);
+    });
+
+
+    $(dropMobile).on('click',function (){
+        $(this).toggleClass('active');
+        $(this).siblings(dropMenuMobile).stop(true).slideToggle();
+    });
+
+    $('.slider__wrap').lightSlider({
+        gallery: false,
+        item: 1,
+        loop:false,
+        slideMargin: 0,
+        controls: true,
+        speed: 600,
+    });
+
+});
